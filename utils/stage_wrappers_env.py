e5f994ff0bdc5833bffafb3000b2e4ea00241ca1
rocholaky <roberto.cholaky@ug.uchile.cl>
2021-07-19 18:27:40 -0400

    Ambiente para la etapa 1 del proyecto

--- utils/stage_wrappers_env.py
+++ utils/stage_wrappers_env.py
@@ -6,9 +6,14 @@ class stage_soccerTraining(wrap.DmGoalWrapper):
 	def __init__(self, team_1, team_2,task_kwargs={}, render_mode_list=None):
 		super().__init__(team_1, team_2, task_kwargs, render_mode_list)
 		# we define the rew_type for this particular stage 2 vs 0
-		self.rew_type = "Stage_1_rew"
-		 
+		self.got_kickable_rew = 0
 		self.current_team_with_ball = None
+		self.old_ball_teammate_dist = []
+
+	@overrides
+	def set_vals(self):
+		super().set_vals()
+		self.got_kickable_rew = False
 		
 	def get_ball(self):
 		'''
@@ -59,7 +64,7 @@ class stage_soccerTraining(wrap.DmGoalWrapper):
 		# we get the teammate position with respect to the ball: 
 		ball_teammate_pos = [-ball_pos[i] - obs[i]["teammate_0_ego_position"]
 										for i in range(self.num_players)]
-		# we get the position of the player and the opponent goal post: 
+		
 		
 
 		# we turn the ball position to polar normalized by the diagonal of the field
@@ -70,10 +75,11 @@ class stage_soccerTraining(wrap.DmGoalWrapper):
 		ball_team_goal_dist = np.array([wrap.polar_mod(ball_team_goal_pos[i]) for i in range(self.num_players)]) / self.max_dist
 		# ball teammate distance: 
 		ball_teammate_dist = np.array([wrap.polar_mod(ball_teammate_pos[i])
-															for i in range(self.num_players)])
+															for i in range(self.num_players)])/self.max_dist
+		
 
-		# indicates if the ball is in a kickable position
-		kickable = ball_dist < self.dist_thresh
+		# indicates if the ball is in a kickable position for any of both players
+		kickable = np.any(ball_dist < self.dist_thresh)
 
 		# rewards of each player in the game 
 		alpha = (int(self.time_limit / self.control_timestep) + 1) / 10
@@ -87,10 +93,27 @@ class stage_soccerTraining(wrap.DmGoalWrapper):
 			# we set the distance of the goalpost and the ball. 
 			delta_D = ((self.old_ball_op_goal_dist - ball_op_goal_dist) \
 						- (self.old_ball_team_goal_dist - ball_team_goal_dist))
-			rewards_scorrer =  (beta -0.1)*kickable_now_first \
-								+ (1-kickable_now_first)*(1.2*delta_D -0.1)\
-								+(1-kickable_now_first)*(1-self.got_kickable_rew)*
-			rewards_pass 	= 
+			delta_ball_d = ball_dist - self.old_ball_dist
+			delta_teammate_ball_d = ball_teammate_dist - self.old_ball_teammate_dist 
+			delta_ball_op_goal_dist = ball_op_goal_dist - self.old_ball_goal_dist
+			
+			kickable_reward = beta
+			still_is_kickable_reward = 1.2*delta_D - np.max(delta_teammate_ball_d) - np.min(delta_ball_d) -0.1
+			other_scenario_reward	= -np.min(delta_ball_d) -np.min(delta_ball_op_goal_dist) -0.1
+
+			rewards += kickable_now_first*kickable_reward \
+						+ (1- self.got_kickable_rew)*kickable*still_is_kickable_reward \
+						+ (1-kickable)*other_scenario_reward
+
+		self.old_ball_dist = ball_dist
+		self.old_ball_op_dist = ball_op_goal_dist
+		self.old_ball_team_goal_dist = ball_team_goal_dist
+		self.old_ball_teammate_dist = ball_teammate_dist
+		self.got_kickable_rew = kickable | self.got_kickable_rew
+
+		return rewards.tolist()
+
+	
 
 
 
