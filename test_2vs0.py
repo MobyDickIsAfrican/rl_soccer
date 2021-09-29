9ae0e071fe5fd0670e85930e29ac0089cce3a832
RICHOLAKY <richolaky@arkenco.cl>
2021-09-29 09:57:45 -0300

    Rc: Agregando los test al repo

--- test_2vs0.py
+++ test_2vs0.py
@@ -4,19 +4,10 @@ import csv
 import dm_soccer2gym
 import numpy as np
 import os
-import path
-import sys
-  
-# directory reach
-directory = path.path(__file__).abspath()
-  
-# setting path
-sys.path.append(directory.parent.parent)
-
 from spinup.utils.test_policy import load_policy_and_env
 from env_2vs0_pass import stage_soccerTraining_pass
 
-
+import torch
 num_runs = 500
 max_ep_len = 600
 min_num = 8e6
@@ -38,7 +29,7 @@ def main(model_path, num, sess=None):
 
     for j in range(num_runs):
             
-        obs = env.reset()
+        obs = torch.Tensor(env.reset()).cuda()
         d = False
         l = 0
         vel1 = []
@@ -47,28 +38,31 @@ def main(model_path, num, sess=None):
         pas2 = []
 
         while not(d or (l == max_ep_len)):
-            a = [get_action(obs[0])]
+            a = get_action(obs[np.newaxis, :]).detach().cpu().numpy()
+            a = [a[0,i, :] for i in range(2)]
             if RENDER:
                 env.render()
             obs, r, d, _ = env.step(a)
+            obs = torch.Tensor(obs).cuda()
             vel1.append(env.timestep.observation[0]['stats_vel_to_ball'])
             vel2.append(env.timestep.observation[1]['stats_vel_to_ball'])
-            pas1.append(env.timestep.observation[0]['stats_i_received_pass'])
-            pas2.append(env.timestep.observation[1]['stats_i_received_pass'])
+            pas1.append(int(env.timestep.observation[0]['stats_i_received_pass']))
+            pas2.append(int(env.timestep.observation[1]['stats_i_received_pass']))
             l += 1
 
         if env.timestep.reward[0] > 0: s += 1
-        elif env.timestep.reward[0] < 0: f += 1
+        elif env.timestep.reward[0] <= 0: f += 1
         ls.append(l)
         vels1.append(np.mean(vel1))
         vels2.append(np.mean(vel2))
-        passes1.append(pas1)
-        passes2.append(pas2)
+        passes1.append(sum(pas1))
+        passes2.append(sum(pas2))
             
         if (j + 1) % 10 == 0: 
-            print(j + 1, s, f)
+            total_pass = np.array(passes1)[-10:]+ np.array(passes2)[-10:]
+            print(j + 1, s, f, np.mean(total_pass), np.max(total_pass), np.min(total_pass))
 
-    return s, ls, vel, 
+    return s, ls, vel1, vel2, passes1, passes2
 
 
 if __name__ == "__main__":
@@ -87,27 +81,26 @@ if __name__ == "__main__":
     orig_path = os.getcwd()
     os.chdir(model_path)
 
-    ckpoint_name = os.path.splitext(os.path.split(args.meta_file)[-1])[0]
+    ckpoint_name = os.path.splitext(os.path.split(args.meta_file)[-1])[0][5:] 
+    print(ckpoint_name)
+    '''
     if len(ckpoint_name) > 11:
         num = int(ckpoint_name[12:])
     else:
         num = 'last'
-    
+
     if type(num) != str and num < min_num:
         
         print(f"Skipping checkpoint number {num}")
-    
+
     else:
+    '''
+    s, ls, vel1, vel2, pas1, pas2 = main(model_path,int(ckpoint_name))
 
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu)
+    with open('stats.csv', 'a+') as f:
+        writer = csv.writer(f)
+        writer.writerow([os.path.split(os.path.abspath(model_path))[1], str(ckpoint_name), str(s / num_runs), 
+                         str(np.mean(ls)), str(np.std(ls)), str(np.mean(vel1)), str(np.std(vel1)),
+                         str(np.mean(vel2)), str(np.std(vel2)), str(np.mean(pas1)), str(np.std(pas1)), str(np.mean(pas2)), str(np.std(pas2))])
 
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-
-        s, ls, vels = main(model_path, num, sess)
-
-        with open('stats.csv', 'a+') as f:
-            writer = csv.writer(f)
-            writer.writerow([os.path.split(os.path.abspath(model_path))[1], str(num), str(s / num_runs), 
-                             str(np.mean(ls)), str(np.std(ls)), str(np.mean(vels)), str(np.std(vels))])
-
-        os.chdir(orig_path)
+    os.chdir(orig_path)
