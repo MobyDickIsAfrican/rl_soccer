57210b783376b21b51e03d6249a2097deb5657cd
rocholaky <roberto.cholaky@ug.uchile.cl>
2021-10-12 17:07:51 -0300

    RC: adding for loop in bash file

--- test_2vs0.sh
+++ test_2vs0.sh
@@ -2,11 +2,13 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate roberto
-#for d in */; do
-d="C:\Users\rocho\RL_proyect\results\2vs0\Join_pass_env"
-for m in $(find $d/pyt_save -name '*.pt'); do
-    echo "python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $PWD/$d --meta_file $m --gpu 0.15"
-    python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $PWD/$d --meta_file $m --gpu 0.15
+for d in $(find "/media/amtc/ab170e70-f9d7-4d20-a751-0c11c1ac74881/roberto/etapa1_results/" -mindepth 1 -maxdepth 1 -type d ); 
+do
+
+    for m in $(find $d/pyt_save -name '*.pt'); do
+        echo "python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $d --meta_file $m --gpu 0.15"
+        python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $d --meta_file $m --gpu 1
+    done
 done
     #fi
 #done
