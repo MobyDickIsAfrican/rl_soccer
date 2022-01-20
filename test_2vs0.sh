5d7620be42faa99f427a40fe9c604e45d828acec
rocholaky <roberto.cholaky@ug.uchile.cl>
2022-01-20 17:23:50 -0300

    RC:  change bash paths

--- test_2vs0.sh
+++ test_2vs0.sh
@@ -6,9 +6,10 @@ for d in $(find "/media/amtc/ab170e70-f9d7-4d20-a751-0c11c1ac74881/roberto/resul
 do
     echo $d
     python /home/amtc/roberto/rl_soccer/get_best.py --model_path $d --success_thres 0.9 --pass_thres 0
-    for m in $(find $d/pyt_save/best_models -name '*.pt'); do
+    m_path = $d"/pyt_save/best_models"
+    for m in $(find $m_path -name '*.pt'); do
         echo "python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $d --meta_file $m --gpu 0.15"
-        python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $d --meta_file $m --gpu 1
+        python /home/amtc/roberto/rl_soccer/test_2vs0.py --model_path $m_path --meta_file $m --gpu 1
     done
 done
     #fi
