9d979f6f94881089e8fa8e7a89d073d6a0c5eeba
rocholaky <roberto.cholaky@ug.uchile.cl>
2022-01-20 17:51:23 -0300

    RC:  change parameters to run bash

--- test_2vs0.py
+++ test_2vs0.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
 
     model_path = args.model_path
     orig_path = os.getcwd()
-    os.chdir(model_path)
+    
 
     ckpoint_name = os.path.splitext(os.path.split(args.meta_file)[-1])[0][5:] 
     print(ckpoint_name)
