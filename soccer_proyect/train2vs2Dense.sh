96d486bc2e5f48da77d344438d04844b8de49441
rocholaky <roberto.cholaky@ug.uchile.cl>
2022-09-13 16:37:15 -0300

    correccion cantidad de instancias para correr

--- soccer_proyect/train2vs2Dense.sh
+++ soccer_proyect/train2vs2Dense.sh
@@ -7,7 +7,7 @@ declare -i window=0
 declare -i pane=0
 session="2vs2_games_$order"
 tmux new-session -d -s $session 
-for ((i=0; i<6; i++)); do
+for ((i=0; i<3; i++)); do
     if [[ $pane -ne 0 ]] 
       then  
          tmux splitw -t $session:$window -d 
