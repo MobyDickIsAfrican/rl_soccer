7b4594def1c842a3fbe4182aa2ddc9feebc22f3f
rocholaky <roberto.cholaky@ug.uchile.cl>
2022-12-14 12:37:33 -0300

    mejoras 1vs0

--- soccer_proyect/exp_1vs0.py
+++ soccer_proyect/exp_1vs0.py
@@ -15,7 +15,7 @@ parser.add_argument("--time_limit", type=float, default=30.)
 args = parser.parse_args()
 
 from spinup.utils.run_utils import setup_logger_kwargs
-logger_kwargs = setup_logger_kwargs(f"td3_soccer_goal_pass_concat_2vs0_{args.control_timestep}", data_dir=args.save_path datestamp=True)
+logger_kwargs = setup_logger_kwargs(f"td3_soccer_goal_pass_concat_2vs0_{args.control_timestep}", data_dir=args.save_path, datestamp=True)
 env_creator = lambda : Env1vs0(1, 0, task_kwargs={'rew_type': 'simple_v2', 'time_limit': 30., 'disable_jump': True, 'dist_thresh': .03, 
                              'control_timestep': 0.1, 'random_state': 69,  "observables": "all"})
 env_test_creator = lambda : Env1vs0(1, 0, task_kwargs={'rew_type': 'simple_v2', 'time_limit': 30., 'disable_jump': True, 'dist_thresh': .03, 
