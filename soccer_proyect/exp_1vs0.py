302cf9712b0c056c8f5fbad7fbd8d273cba4998a
rocholaky <roberto.cholaky@ug.uchile.cl>
2022-12-14 12:29:27 -0300

    mejora save_path 1vs0

--- soccer_proyect/exp_1vs0.py
+++ soccer_proyect/exp_1vs0.py
@@ -5,6 +5,7 @@ from TD3_team_alg_concat import TD3_team_alg
 from env_2vs0_pass import stage_soccerTraining_pass
 from env1vs0 import Env1vs0
 parser = argparse.ArgumentParser()
+parser.add_argument('--save_path', type=str, default="/results/1vs0Train")
 parser.add_argument('--seed', '-s', type=int, default=0)
 parser.add_argument('--exp_name', type=str, default='td3_soccer')
 parser.add_argument('--gamma', type=float, default=0.99)
@@ -12,10 +13,9 @@ parser.add_argument('--epochs', type=int, default=2000)
 parser.add_argument("--control_timestep", type=float, default=0.1)
 parser.add_argument("--time_limit", type=float, default=30.)
 args = parser.parse_args()
-args = parser.parse_args()
 
 from spinup.utils.run_utils import setup_logger_kwargs
-logger_kwargs = setup_logger_kwargs(f"td3_soccer_goal_pass_concat_2vs0_{args.control_timestep}", data_dir="results/1vs0Train", datestamp=True)
+logger_kwargs = setup_logger_kwargs(f"td3_soccer_goal_pass_concat_2vs0_{args.control_timestep}", data_dir=args.save_path datestamp=True)
 env_creator = lambda : Env1vs0(1, 0, task_kwargs={'rew_type': 'simple_v2', 'time_limit': 30., 'disable_jump': True, 'dist_thresh': .03, 
                              'control_timestep': 0.1, 'random_state': 69,  "observables": "all"})
 env_test_creator = lambda : Env1vs0(1, 0, task_kwargs={'rew_type': 'simple_v2', 'time_limit': 30., 'disable_jump': True, 'dist_thresh': .03, 
