8aaafe3f6ea4985455a9c5563a9d5439d7266cab
RICHOLAKY <richolaky@arkenco.cl>
2023-01-08 23:10:00 -0300

    cambios run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name mujocoSoccer -v /home/roberto.cholaky/cachefs:/results -v /home/roberto.cholaky/rl_soccer/models:/models  --gpus device=1 -t -d mujocosoccerimage
+docker run --name mujocoSoccer -v /home/roberto.cholaky/cachefs:/results -v /home/roberto.cholaky/rl_soccer/models:/models  --cpuset-cpus 1-20 --gpus device=1 -t -d mujocosoccerimage
